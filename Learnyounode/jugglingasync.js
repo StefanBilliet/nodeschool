bcd32c101492e4552a8cbc23beb04053b1fbab8a
Stefan Billiet <stefanbilliet@me.com>
2013-11-14 10:06:42 +0100

    Refactored solution for jugglingasync

--- Learnyounode/jugglingasync.js
+++ Learnyounode/jugglingasync.js
@@ -1,34 +1,21 @@
-(function (url1, url2, url3) {
+(function () {
 	var http = require("http");
 	var Q = require("q");
 	var bl = require("bl");
 
-	var getUrl1Promise = Q.defer();
-	http.get(url1, function (response) {
-		response.pipe(bl(function (error, data) {
-			if(error)
-				getUrl1Promise.reject(error);
-			getUrl1Promise.resolve(data.toString());
-		}));	
-	});
-	var getUrl2Promise = Q.defer();
-	http.get(url2, function (response) {
-		response.pipe(bl(function (error, data) {
-			if(error)
-				getUrl2Promise.reject(error);
-			getUrl2Promise.resolve(data.toString());
-		}));
-	});
-	var getUrl3Promise = Q.defer();
-	http.get(url3, function (response) {
-		response.pipe(bl(function (error, data) {
-			if(error)
-				getUrl3Promise.reject(error);
-			getUrl3Promise.resolve(data.toString());
-		}));
+	var promises = Array.prototype.slice.call(arguments, 0).map(function (url) {
+		var getUrlDeferred = Q.defer();
+		http.get(url, function (response) {
+			response.pipe(bl(function (error, data) {
+				if(error)
+					getUrlDeferred.reject(error);
+				getUrlDeferred.resolve(data.toString());
+			}));	
+		});
+		return getUrlDeferred.promise;
 	});
 
-	Q.all([getUrl1Promise.promise, getUrl2Promise.promise, getUrl3Promise.promise]).
+	Q.all(promises).
 	spread(function (data1, data2, data3) {
 		console.log(data1);
 		console.log(data2);
